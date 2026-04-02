864fa52a049326867cd2434317acf39630afebb6
juancobis <jcobis.dev@gmail.com>
2026-04-02 17:34:31 -0400

    Create popup and style it

--- script.js
+++ script.js
@@ -2,24 +2,21 @@ const divBooks = document.querySelector('.books')
 const btnNewBook = document.querySelector('.btn-new-book')
 
 class Book {
-  constructor(title, author, year, ...genres) {
+  constructor(title, author, year) {
     this.id = crypto.randomUUID()
     this.title = title
     this.author = author
     this.year = year
-    this.genres = [...genres]
   }
 }
 
 const myLibrary = []
 
 const addBookToLibrary = (title) => {
-  myLibrary.push(new Book('1984', 'George Orwell', 1949, 'Novel', 'Fiction'))
+  myLibrary.push(new Book('1984', 'George Orwell', 1949))
 }
 
-btnNewBook.addEventListener('click', () => {
-  
-})
+btnNewBook.addEventListener('click', () => {})
 
 addBookToLibrary()
 
