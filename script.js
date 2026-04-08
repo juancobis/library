e5c3dc729c0140cf935b98a92b16f2a428aede66
juancobis <jcobis.dev@gmail.com>
2026-04-08 17:15:33 -0400

    Add number of pages

--- script.js
+++ script.js
@@ -1,44 +1,38 @@
 const inputBookTitle = document.querySelector('#input-book-title')
 const inputBookAuthor = document.querySelector('#input-book-author')
 const inputBookYear = document.querySelector('#input-book-year')
+const inputBookPages = document.querySelector('#input-book-pages')
 const form = document.querySelector('form')
 const divBooks = document.querySelector('.books')
 
 class Book {
-  constructor(title, author, year) {
+  constructor(title, author, year, pages) {
     this.id = crypto.randomUUID()
     this.title = title
     this.author = author
     this.year = year
+    this.pages = pages
   }
 }
 
 const myLibrary = []
 myLibrary.push(new Book('1984', 'George Orwell', 1949))
 
-const addBookToLibrary = (title, author, year) => {
+const addBookToLibrary = (title, author, year, pages) => {
   if (
     typeof title !== 'string' ||
     typeof author !== 'string' ||
-    typeof year !== 'number'
+    !Number.isInteger(year) ||
+    year < 0 ||
+    !Number.isInteger(pages) ||
+    pages < 0
   ) {
     console.error('niño malo')
     return
   }
 
-  myLibrary.push(new Book(title, author, year))
+  myLibrary.push(new Book(title, author, year, pages))
 }
-
-form.addEventListener('submit', (e) => {
-  e.preventDefault()
-
-  const title = inputBookTitle.value
-  const author = inputBookAuthor.value
-  const year = +inputBookYear.value
-
-  addBookToLibrary(title, author, year)
-  renderBooks()
-})
 
 const renderBooks = () => {
   divBooks.innerHTML = ''
@@ -50,12 +44,23 @@ const renderBooks = () => {
       <h3>${book.title}</h3>
       <p>${book.author}</p>
       <p>${book.year}</p>
+
+      <small>${book.pages}</small>
     `
 
-    bookCard.classList.add('card', 'book')
-
+    bookCard.classList.add('card')
     divBooks.appendChild(bookCard)
-
-    console.log(book)
   })
 }
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault()
+
+  const title = inputBookTitle.value
+  const author = inputBookAuthor.value
+  const year = +inputBookYear.value
+  const pages = +inputBookPages.value
+
+  addBookToLibrary(title, author, year, pages)
+  renderBooks()
+})
